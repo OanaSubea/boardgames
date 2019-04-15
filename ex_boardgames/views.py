2c51756014295cc9d0129857c7483c5eefd498a2
OanaSubea <subeaoana@gmail.com>
2019-04-15 20:14:45 +0300

    add parameter to template

--- ex_boardgames/views.py
+++ ex_boardgames/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, "main/home.html") #main-ul din templates
+    return render(request, "main/home.html",{'message':'Hi, there!'})#main-ul din templates
